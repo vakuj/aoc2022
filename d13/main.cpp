761ab30da8da13d7899ada76dbbe2ff69cd2a402
vakuj <vakujrvi@gmail.com>
2022-12-17 14:02:33 +0100

    added more helpers. passes all but one of the tests currently. thinking recursion is the key.

--- d13/main.cpp
+++ d13/main.cpp
@@ -3,6 +3,8 @@
 #include <algorithm>
 #include <list>
 #include <stack>
+#include <map>
+#include <string>
 #include <cstring>
 #include <cassert>
 
@@ -11,17 +13,71 @@ using namespace std;
 #define BUFSIZE 1024
 #define NUMSCAP 12
 #define EMPTY -1
-/** used in processing */
 
+/** item types */
+enum itype_t
+{
+    CONSTANT,   // integer constant
+    LIST_START, // marker for list start, '['
+    LIST_STOP,  // marker for list stop, ']'
+    DELIMITER,  // item seperator, ','
+    NONE_ITEM,  // empty list marker
+};
+/** Comparison pass/fail conditions */
+enum check_t
+{
+    UNDEFINED,    // default, none of the other cases match
+    TRUE_INT,     // lhs < rhs
+    TRUE_LIST,    // lhs[n] < rhs[n]
+    TRUE_LENGTH,  // lhs.length < rhs.length
+    FALSE_INT,    // lhs > rhs
+    FALSE_LIST,   // lhs[n] > rhs[n]
+    FALSE_LENGTH, // lhs.length > rhs.length
+};
+/** Convert char to itype_t, supports LIST_START, LIST_STOP and DELIMITER */
+static map<char, itype_t> char_to_itype = {
+    {'[', itype_t::LIST_START},
+    {']', itype_t::LIST_STOP},
+    {',', itype_t::DELIMITER},
+};
+/** Convert itype_t to char, supports LIST_START, LIST_STOP, DELIMITER and NONE_ITEM */
+static map<itype_t, char> itype_to_char = {
+    {itype_t::LIST_START, '['},
+    {itype_t::LIST_STOP, ']'},
+    {itype_t::DELIMITER, ','},
+    {itype_t::NONE_ITEM, '_'},
+};
+/** inverts the order of check, i.e., (lhs < rhs) -> (lhs > rhs). Undefined is still undefined. */
+static map<check_t, check_t> invert_check = {
+    {check_t::UNDEFINED, check_t::UNDEFINED},
+    {check_t::TRUE_INT, check_t::FALSE_INT},
+    {check_t::TRUE_LIST, check_t::FALSE_LIST},
+    {check_t::TRUE_LENGTH, check_t::FALSE_LENGTH},
+    {check_t::FALSE_INT, check_t::TRUE_INT},
+    {check_t::FALSE_LIST, check_t::TRUE_LIST},
+    {check_t::FALSE_LENGTH, check_t::TRUE_LENGTH},
+};
+static map<check_t, string> check_to_string = {
+    {check_t::UNDEFINED, "check_t::UNDEFINED"},
+    {check_t::TRUE_INT, "check_t::TRUE_INT"},
+    {check_t::TRUE_LIST, "check_t::TRUE_LIST"},
+    {check_t::TRUE_LENGTH, "check_t::TRUE_LENGTH"},
+    {check_t::FALSE_INT, "check_t::FALSE_INT"},
+    {check_t::FALSE_LIST, "check_t::FALSE_LIST"},
+    {check_t::FALSE_LENGTH, "check_t::FALSE_LENGTH"},
+};
+/** list item */
 typedef struct item_t
 {
-    int32_t value;
-    int32_t level;
+    int32_t value; // value of item
+    itype_t itype; // item type of item
+
 } item_t;
+/** data packet content */
 typedef struct packet_t
 {
-    list<item_t> lhs;
-    list<item_t> rhs;
+    list<item_t> lhs; // left-hand-side of packet
+    list<item_t> rhs; // right-hand-side of packet
 } packet_t;
 list<packet_t> *data;
 int32_t cnt;
@@ -33,14 +89,18 @@ void parsePacket(const char *cstr, size_t count, list<item_t> *items)
     assert(items != NULL); // Panic if items is NULL
     assert(cstr != NULL);  // Panic if cstr is NULL
 
-    char *num = new char[NUMSCAP];     // temporary holder for integers as chars
-    char *ptr = num;                   // pointer to where to store next integer in num;
-    items->clear();                    // clear items to ensure empty;
-    item_t item = {0, 0};              // current item temporary holder
-    for (size_t i = 0; i < count; i++) // iterate through count chars and parse to item
+    char *num = new char[NUMSCAP];        // temporary holder for integers as chars
+    char *ptr = num;                      // pointer to where to store next integer in num;
+    items->clear();                       // clear items to ensure empty;
+    item_t item = {0, itype_t::CONSTANT}; // current item temporary holder
+    for (size_t i = 0; i < count; i++)    // iterate through count chars and parse to item
     {
-        if (cstr[i] == '[')      // list marker start detected
-            item.level++;        // increase item level
+        if (cstr[i] == '[') // list marker start detected
+        {
+            item.value = 0;
+            item.itype = char_to_itype[cstr[i]];
+            items->push_back(item);
+        }
         else if (cstr[i] == ']') // list marker end detected
         {
             /**
@@ -50,6 +110,7 @@ void parsePacket(const char *cstr, size_t count, list<item_t> *items)
             if (ptr != num)
             {
                 item.value = atoi(num);
+                item.itype = itype_t::CONSTANT;
                 memset(num, '\0', NUMSCAP);
                 ptr = num;
                 items->push_back(item);
@@ -61,9 +122,13 @@ void parsePacket(const char *cstr, size_t count, list<item_t> *items)
             if (cstr[i - 1] == '[')
             {
                 item.value = EMPTY;
+                item.itype = itype_t::NONE_ITEM;
                 items->push_back(item);
             }
-            item.level--; // decrease item level
+            item.value = 0;
+            item.itype = char_to_itype[cstr[i]];
+            ;
+            items->push_back(item);
         }
         else if (cstr[i] == ',')
         {
@@ -71,10 +136,14 @@ void parsePacket(const char *cstr, size_t count, list<item_t> *items)
             if (ptr != num)
             {
                 item.value = atoi(num);
+                item.itype = itype_t::CONSTANT;
                 memset(num, '\0', NUMSCAP);
                 ptr = num;
                 items->push_back(item);
             }
+            item.value = 0;
+            item.itype = char_to_itype[cstr[i]];
+            items->push_back(item);
         }
         else
         {
@@ -140,23 +209,124 @@ void write_output(const char *filename, int32_t task1, int32_t task2)
     ofs << "Task 2 result: " << task2 << "\n";
     ofs.close();
 }
+void print_list(list<item_t> *items)
+{
+    for (auto lit = items->begin(); lit != items->end(); lit++)
+    {
+        if (lit->itype == CONSTANT)
+            cout << lit->value;
+        else
+            cout << itype_to_char[lit->itype];
+    }
+    cout << "\n";
+}
 void process_task()
 {
     /** Do something to process the task in general */
-    // for (auto it = data->begin(); it != data->end(); it++)
-    // {
-    //     for (auto lit = it->lhs.begin(); lit != it->lhs.end(); lit++)
-    //     {
-    //         cout.width(lit->level);
-    //         cout << lit->value << "\n";
-    //     }
-    //     for (auto rit = it->rhs.begin(); rit != it->rhs.end(); rit++)
-    //     {
-    //         cout.width(rit->level);
-    //         cout << rit->value << "\n";
-    //     }
-    // }
+    for (auto it = data->begin(); it != data->end(); it++)
+    {
+        print_list(&(it->lhs));
+        print_list(&(it->rhs));
+        cout << "\n";
+    }
 }
+void fetch_next(list<item_t>::iterator &it, list<item_t> *dst)
+{
+    assert(dst != NULL);
+    dst->clear();
+    if (it->itype == itype_t::CONSTANT)
+    {
+        dst->push_back(*it);
+        it++;
+    }
+    else if (it->itype == itype_t::LIST_START)
+    {
+        it++;
+        int32_t balance = 1;
+        while (balance != 0)
+        {
+            if (it->itype == itype_t::LIST_START)
+                balance++;
+            if (it->itype == itype_t::LIST_STOP)
+                balance--;
+
+            dst->push_back(*it);
+            it++;
+        }
+        dst->pop_back();
+    }
+    else if (it->itype == itype_t::LIST_STOP)
+    {
+        it++;
+    }
+    else if (it->itype == itype_t::DELIMITER)
+    {
+        it++;
+    }
+}
+
+check_t item_compare(item_t left, item_t right)
+{
+    if (left.itype == itype_t::CONSTANT && right.itype == itype_t::CONSTANT)
+    {
+        if (left.value < right.value)
+            return check_t::TRUE_INT;
+        if (left.value > right.value)
+            return check_t::FALSE_INT;
+        return check_t::UNDEFINED;
+    }
+    if (left.itype == itype_t::CONSTANT && right.itype == itype_t::NONE_ITEM)
+        return check_t::FALSE_LENGTH;
+    if (left.itype == itype_t::NONE_ITEM && right.itype == itype_t::CONSTANT)
+        return check_t::TRUE_LENGTH;
+    return check_t::UNDEFINED;
+}
+/**
+ * @brief compare list against item, where list and item order is specified by
+ *        left_to_right
+ *
+ * @param items list of items to compare against item
+ * @param item item to compare against list of items
+ * @param left_to_right set true if list is on lhs, else false for rhs
+ * @return check_t the outcome of the comparison, flipped to correct order as specified by left_to_right
+ */
+check_t list_to_item_compare(list<item_t> *items, item_t item, bool left_to_right)
+{
+    check_t check = check_t::UNDEFINED;
+    for (auto it = items->begin(); it != items->end(); it++)
+    {
+        check = item_compare(*it, item);
+        if (check != check_t::UNDEFINED)
+            break;
+    }
+    if (!left_to_right)
+        return invert_check[check];
+    return check;
+}
+check_t list_to_list_compare(list<item_t> *left, list<item_t> *right)
+{
+    check_t check = check_t::UNDEFINED;
+    list<item_t>::iterator lit, rit;
+    lit = left->begin();
+    rit = right->begin();
+    while (lit != left->end() && rit != right->end())
+    {
+        check = item_compare(*lit, *rit);
+        lit++;
+        rit++;
+        if (check != check_t::UNDEFINED)
+            break;
+    }
+    if (check == check_t::UNDEFINED)
+    {
+        if (lit == left->end() && rit != right->end())
+            check = check_t::TRUE_LENGTH;
+        if (lit != left->end() && rit == right->end())
+            check = check_t::FALSE_LENGTH;
+    }
+    return check;
+}
+
 int32_t process_task1(void)
 {
     /**
@@ -173,106 +343,57 @@ int32_t process_task1(void)
      * If exactly one value is an integer, convert the integer to a list which contains that integer as its only value, then retry the comparison.
      *  - For example, if comparing `[0,0,0]` and `2`, convert the right value to `[2]`; the result is then found by instead comparing `[0,0,0]` and `[2]`.
      */
-    enum check_t
-    {
-        UNDEFINED,
-        TRUE_INT,
-        TRUE_LIST,
-        TRUE_LENGTH,
-        FALSE_INT,
-        FALSE_LIST,
-        FALSE_LENGTH,
-    };
-    packet_t front;
-    stack<item_t> lstack, rstack;
-    item_t litem, ritem;
-    int32_t current_level = 1;
-    bool right_order;
     check_t check = check_t::UNDEFINED;
+    packet_t front;
+    list<item_t> lproc, rproc;
+    list<item_t>::iterator lit, rit;
     int32_t ctr = 1;
+    int32_t sum_of_true = 0;
     while (!data->empty())
     {
-        right_order = false;
         check = check_t::UNDEFINED;
         front = data->front();
         data->pop_front();
-        while (!front.lhs.empty() && !front.rhs.empty())
+
+        // remove starting marker
+        lit = front.lhs.begin();
+        rit = front.rhs.begin();
+        lit++;
+        rit++;
+
+        while (lit != front.lhs.end() && rit != front.rhs.end())
         {
-            litem = front.lhs.front();
-            ritem = front.rhs.front();
-            front.lhs.pop_front();
-            front.rhs.pop_front();
-            if (litem.level == ritem.level)
-            {
-                if (litem.value < ritem.value)
-                {
-                    check = check_t::TRUE_INT;
-                    break;
-                }
-                if (litem.value > ritem.value)
-                {
-                    check = check_t::FALSE_INT;
-                    break;
-                }
-            }
-            else
-            {
-                if (litem.value == EMPTY && ritem.value == EMPTY)
-                {
-                    if (litem.level < ritem.level)
-                    {
-                        check = check_t::TRUE_LIST;
-                        break;
-                    }
-                    else
-                    {
-                        check = check_t::FALSE_LIST;
-                        break;
-                    }
-                }
-                else if (litem.level < ritem.level)
-                {
-                    /** probably should advance rhs */
-                    if (litem.value < ritem.value)
-                    {
-                        check = check_t::TRUE_INT;
-                        break;
-                    }
-                    if (litem.value > ritem.value)
-                    {
-                        check = check_t::FALSE_INT;
-                        break;
-                    }
-                }
-                else
-                {
-                    /** probably should advance lhs */
-                    if (litem.value < ritem.value)
-                    {
-                        check = check_t::TRUE_INT;
-                        break;
-                    }
-                    if (litem.value > ritem.value)
-                    {
-                        check = check_t::FALSE_INT;
-                        break;
-                    }
-                }
-            }
+            fetch_next(lit, &lproc);
+            fetch_next(rit, &rproc);
+            // print_list(&lproc);
+
+            if (lproc.size() == 1 && rproc.size() == 1) // each hold a constant
+                check = item_compare(lproc.front(), rproc.front());
+            else if (lproc.size() == 1 && rproc.size() > 1)
+                check = list_to_item_compare(&rproc, lproc.front(), false);
+            else if (lproc.size() > 1 && rproc.size() == 1)
+                check = list_to_item_compare(&lproc, rproc.front(), true);
+            else if (lproc.size() > 1 && rproc.size() > 1)
+                check = list_to_list_compare(&lproc, &rproc);
+            // else // empty
+            if (check != check_t::UNDEFINED)
+                break;
         }
         if (check == check_t::UNDEFINED)
         {
-            if (front.lhs.empty() && !front.rhs.empty())
+            if (lit == front.lhs.end() && rit != front.rhs.end())
                 check = check_t::TRUE_LENGTH;
-            else
+            if (lit != front.lhs.end() && rit == front.rhs.end())
                 check = check_t::FALSE_LENGTH;
         }
+        print_list(&front.lhs);
+        print_list(&front.rhs);
+        cout << check_to_string[check] << "\n\n";
         if (check > check_t::UNDEFINED && check <= check_t::TRUE_LENGTH)
-            cout << ctr << ", ";
+            sum_of_true += ctr;
         ctr++;
     }
-    cout << "\n";
-    return 0;
+    return sum_of_true;
 }
 int32_t process_task2(void)
 {
@@ -282,12 +403,12 @@ int32_t process_task2(void)
 int main()
 {
     /** use this for testing examples */
-    // read_input("test.txt");
+    read_input("test.txt");
 
     /** use this to run input */
-    read_input("input.txt");
+    // read_input("input.txt");
 
-    process_task();
+    // process_task();
 
     int32_t task1 = process_task1();
     int32_t task2 = process_task2();
