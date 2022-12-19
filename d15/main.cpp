532b4e10ac640e222ac03c089b7e1d8695422d24
vakuj <vakujrvi@gmail.com>
2022-12-19 11:16:41 +0100

    d15 task 2 done. perhaps not optimal solution for speed but is still a reduced set solution.

--- d15/main.cpp
+++ d15/main.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 #include <fstream>
 #include <algorithm>
-#include <vector>
+
 #include <list>
 #include <set>
-#include <map>
+
 #include <cassert>
 using namespace std;
 #define BUFSIZE 1024
 /** used in processing */
+/** coordinate data type with x and y*/
 typedef struct coord_t
 {
     int32_t x;
     int32_t y;
 } coord_t;
+/** sensor and beacon pair coordinates, includes distance between the pair */
 typedef struct pair_t
 {
     coord_t sensor;
@@ -51,7 +53,6 @@ void read_input(const char *filename)
         }
         else
         {
-
             /** store for later processing */
             ifs.getline(indata, BUFSIZE);
             if (4 != sscanf(indata, "Sensor at x=%d, y=%d: closest beacon is at x=%d, y=%d", &sb.sensor.x, &sb.sensor.y, &sb.beacon.x, &sb.beacon.y))
@@ -65,7 +66,7 @@ void read_input(const char *filename)
     delete[] indata;
 }
 
-void write_output(const char *filename, size_t task1, int32_t task2)
+void write_output(const char *filename, size_t task1, size_t task2)
 {
     ofstream ofs(filename, ofstream::out);
     ofs << "Task 1 result: " << task1 << "\n";
@@ -112,61 +113,114 @@ size_t find_impossible_at_y(int32_t target_y)
     }
     return impossible.size();
 }
+inline bool inBounds(coord_t c, int32_t upper_bound)
+{
+    return ((0 <= c.x && c.x < upper_bound) && (0 <= c.y && c.y < upper_bound));
+}
 coord_t find_impossible_coord(int32_t upper_bound)
 {
-    map<int32_t, map<int32_t, bool>> impossible;
+    /**
+     * Idea:
+     *  Add the data points just outside of the current
+     *  boundary as shown below (the 1s, 2s, 3s and 4s).
+     *  These will be data points will serve as possible
+     *  candidates as possible locations where the can
+     *  beacon exists.
+     *
+     *    2nd   1   1st
+     *         2#1
+     *        2###1
+     *       2#####1
+     *      2###X###4
+     *       3#####4
+     *        3###4
+     *         3#4
+     *    3rd   3   4th
+     *
+     *  When the set of possibles have been created, iterate
+     *  through each point and compare distance to all sensors.
+     *  If the current point is longer away from each of the
+     *  sensors than their individual reach (sensor->beacon distance)
+     *  then the current point is the one which holds the missing
+     *  beacon.
+     */
+
+    list<coord_t> possible;
     coord_t current = {0, 0};
+    // find and add all boundary elements as possible candidates
     for (auto it : *data)
     {
-        for (int32_t dx = 0; dx < it.distance * 2; dx++)
+        current = it.sensor;
+        current.x += it.distance + 1;
+        // add boundary of 1st quadrant
+        for (int32_t dx = 0; dx < it.distance + 1; dx++)
         {
-            for (int32_t dy = 0; dy < it.distance * 2; dy++)
-            {
-                current.x = it.sensor.x - it.distance + dx;
-                current.y = it.sensor.y - it.distance + dy;
-                if ((0 <= current.x && current.x < upper_bound) && (0 <= current.y && current.y < upper_bound))
-                    if (abs_distance(current, it.sensor) <= it.distance)
-                        impossible[current.x][current.y] = true;
-            }
+            current.x--;
+            current.y++;
+            if (inBounds(current, upper_bound))
+                possible.push_back(current);
+        }
+        // add boundary of 2nd quadrant
+        for (int32_t dx = 0; dx < it.distance + 1; dx++)
+        {
+            current.x--;
+            current.y--;
+            if (inBounds(current, upper_bound))
+                possible.push_back(current);
+        }
+        // add boundary of 3rd quadrant
+        for (int32_t dx = 0; dx < it.distance + 1; dx++)
+        {
+            current.x++;
+            current.y--;
+            if (inBounds(current, upper_bound))
+                possible.push_back(current);
+        }
+        // add boundary of 4th quadrant
+        for (int32_t dx = 0; dx < it.distance + 1; dx++)
+        {
+            current.x++;
+            current.y++;
+            if (inBounds(current, upper_bound))
+                possible.push_back(current);
         }
     }
     current.x = 0;
     current.y = 0;
-    for (int32_t x = 0; x < upper_bound; x++)
+    bool candidate;
+    for (auto pit : possible)
     {
-        if (impossible.count(x) < (size_t)upper_bound)
+        candidate = true; // assume this is the "one"
+        for (auto it : *data)
         {
-            current.x = x;
-            for (int32_t y = 0; y < upper_bound; y++)
-            {
-                if (impossible[x].count(y) == 0)
-                {
-                    current.y = y;
-                    break;
-                }
-            }
+            if (abs_distance(pit, {it.sensor}) <= it.distance)
+                candidate = candidate ? false : candidate; // turns out the initial assumption was wrong.
         }
-        if (current.y != 0)
+        if (candidate)
+        {
+            // assumption was correct -> beacon found!
+            current = pit;
             break;
+        }
     }
     return current;
 }
 size_t process_task1(void)
 {
-    // return find_impossible_at_y(10);
     return find_impossible_at_y(2000000);
 }
-int32_t process_task2(void)
+size_t process_task2(void)
 {
-    /** Do something to process the task specific to 2 */
-    // coord_t c = find_impossible_coord(20);
     coord_t c = find_impossible_coord(4000000);
-    return c.x * 4000000 + c.y;
+    return ((size_t)c.x * 4000000 + (size_t)c.y);
 }
 int main()
 {
     /** use this for testing examples */
     // read_input("test.txt");
+    // size_t task1 = find_impossible_at_y(10);
+    // coord_t c = find_impossible_coord(20);
+    // size_t task2 = ((size_t)c.x * 4000000 + (size_t)c.y);
 
     /** use this to run input */
     read_input("input.txt");
@@ -174,7 +228,7 @@ int main()
     process_task();
 
     size_t task1 = process_task1();
-    int32_t task2 = process_task2();
+    size_t task2 = process_task2();
 
     /** store output to file */
     write_output("output.txt", task1, task2);
