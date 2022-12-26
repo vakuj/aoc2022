90b4dc158b7f722f40474764a59adadf56690e25
vakuj <vakujrvi@gmail.com>
2022-12-26 12:49:24 +0100

    D17 task 1 done.

--- d17/main.cpp
+++ d17/main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 #define BUFSIZE 1024
 
-#define cave_t map<int32_t, map<int32_t, bool>>
+#define cave_t vector<vector<bool>>
 typedef struct coord_t
 {
     int32_t x;
@@ -35,12 +35,20 @@ static map<dir_t, char> dir_to_char = {
     {RIGHT, '>'},
     {DOWN, 'v'},
 };
-static map<rock_t, vector<coord_t>> shape_map = {
-    {HLINE, {{0, 0}, {1, 0}, {2, 0}, {3, 0}}},
-    {CROSS, {{1, 0}, {1, 1}, {1, 2}, {0, 1}, {2, 1}}},
-    {LSHAPE, {{0, 0}, {1, 0}, {2, 0}, {2, 1}, {2, 2}}},
-    {VLINE, {{0, 0}, {0, 1}, {0, 2}, {0, 3}}},
-    {SQUARE, {{0, 0}, {0, 1}, {1, 1}, {1, 0}}},
+static map<rock_t, char> rock_fill_map = {
+    {HLINE, '-'},
+    {CROSS, '+'},
+    {LSHAPE, 'L'},
+    {VLINE, 'I'},
+    {SQUARE, '#'},
+};
+static map<rock_t, vector<coord_t>>
+    shape_map = {
+        {HLINE, {{0, 0}, {1, 0}, {2, 0}, {3, 0}}},
+        {CROSS, {{1, 0}, {1, 1}, {1, 2}, {0, 1}, {2, 1}}},
+        {LSHAPE, {{0, 0}, {1, 0}, {2, 0}, {2, 1}, {2, 2}}},
+        {VLINE, {{0, 0}, {0, 1}, {0, 2}, {0, 3}}},
+        {SQUARE, {{0, 0}, {0, 1}, {1, 1}, {1, 0}}},
 };
 static map<rock_t, coord_t> grid_size = {
     {HLINE, {4, 1}},
@@ -57,135 +65,152 @@ private:
     bool frozen; // true if rock is frozen cave
 
 public:
+    /**
+     * @brief Construct a new ROCK object
+     * defaults to rock_t::HLINE and position x=2, y=4
+     */
     ROCK()
     {
-        spawn_rock(rock_t::CROSS, {0, 0});
+        spawn_rock(rock_t::HLINE, {2, 4});
     }
+    /**
+     * @brief Construct a new ROCK object with specified rock type and position
+     *
+     * @param nrock the rock type of the new rock
+     * @param spos positon of the new rock
+     */
     ROCK(rock_t nrock, coord_t spos)
     {
         spawn_rock(nrock, spos);
     }
+    /**
+     * @brief Destroy the ROCK object
+     *
+     */
     ~ROCK() {}
+    /**
+     * @brief Spawns a rock of type nrock at position spos, is called by ctors
+     *
+     * @param nrock the rock type of the spawned rock
+     * @param spos the spawn position
+     */
     void spawn_rock(rock_t nrock, coord_t spos)
     {
         frozen = false;
         rock = nrock;
         pos = spos;
     }
-    coord_t block_by_wall(coord_t npos)
+    /**
+     * @brief returns the state of member variable frozen
+     *
+     * @return true rock is frozen cannot move anymore
+     */
+    bool isFrozen(void) { return frozen; }
+    /**
+     * @brief returns the current positon of rock
+     *
+     * @return coord_t x,y coordinates of the rock
+     */
+    coord_t getPos(void) { return pos; }
+    /**
+     * @brief returns the highest point of the rock object
+     *
+     * @return int32_t the highest point (y) of rock.
+     */
+    int32_t getHighest(void) { return pos.y + grid_size[rock].y - 1; }
+    /**
+     * @brief  checks if the new position npos is possible based on the cave layout
+     * should only be checked if moving side-ways.
+     *
+     * @param npos new position to check
+     * @param cave the cave layout
+     * @return true if the new position is blocked
+     */
+    bool is_blocked_movement(coord_t npos, cave_t cave)
     {
-        coord_t grid = grid_size[rock];
+        // blocked by left wall?
         if (npos.x < 0)
-            npos.x = 0;
-        if (npos.x + grid.x > 7)
-            npos.x--;
-        return npos;
+            return true;
+        // blocked by right wall?
+        if (npos.x + grid_size[rock].x > 7)
+            return true;
+        // blocked by other frozen rock?
+        for (auto it : shape_map[rock])
+            if (cave[it.x + npos.x][it.y + npos.y])
+                return true;
+
+        return false;
     }
-    coord_t block_by_frozen(coord_t npos, dir_t dir, map<int32_t, int32_t> cfloor)
-    {
-        if (dir != dir_t::DOWN)
-        {
-            vector<coord_t> shape = shape_map[rock];
-            for (auto it : shape)
-            {
-                if (npos.y + it.y <= cfloor[it.x + npos.x])
-                {
-                    switch (dir)
-                    {
-                    case dir_t::LEFT:
-                        npos.x++;
-                        break;
-                    case dir_t::RIGHT:
-                        npos.x--;
-                        break;
-                    default:
-                        break;
-                    }
-                }
-            }
-        }
-        return npos;
-    }
+    /**
+     * @brief checks if the new position of the rock has collided with anything.
+     * should only be checked if moving down.
+     *
+     * @param npos
+     * @param cave
+     * @return true
+     * @return false
+     */
     bool collision_rock(coord_t npos, cave_t cave)
     {
-        vector<coord_t> shape = shape_map[rock];
-        bool collision = false;
-        for (auto it : shape)
+        for (auto it : shape_map[rock])
         {
-            if (cave.count(it.x + npos.x) != 0)
-                if (cave[it.x + npos.x].count(it.y + npos.y) != 0)
-                    collision = true;
-            if (collision)
-                break;
-        }
-        return collision;
-    }
-    bool collision_floor(coord_t npos, dir_t dir, map<int32_t, int32_t> cfloor)
-    {
-        if (dir == dir_t::DOWN)
-        {
-            vector<coord_t> shape = shape_map[rock];
-            for (auto it : shape)
-            {
-                if (cfloor[npos.x + it.x] >= npos.y + it.y)
-                    return true;
-            }
+            if (cave[it.x + npos.x][it.y + npos.y])
+                return true;
         }
         return false;
     }
+    /**
+     * @brief Sets the frozen position of the rock in cave. Also sets frozen to true.
+     *
+     * @param cave the pointer to the cave where the rock should be frozen in place.
+     */
     void setFrozenPos(cave_t *cave)
     {
-        if (frozen)
+        frozen = true;
+        for (auto it : shape_map[rock])
+            cave->at(it.x + pos.x)[it.y + pos.y] = true;
+    }
+    /**
+     * @brief Moves the rock based on dir and updates cave accordingly. Does nothing if rock already frozen.
+     *
+     * @param dir the direction in which the rock should move.
+     * @param cave the pointer to the cave where the rock moves.
+     */
+    void move_rock(dir_t dir, cave_t *cave)
+    {
+        if (!frozen)
         {
-            vector<coord_t> shape = shape_map[rock];
-            for (auto it : shape)
+            coord_t npos = pos;
+            switch (dir)
             {
-                if (cave->count(it.x + pos.x) != 0)
-                    cave->at(it.x + pos.x)[it.y + pos.y] = true;
+            case dir_t::LEFT:
+                npos.x--;
+                break;
+            case dir_t::RIGHT:
+                npos.x++;
+                break;
+            case dir_t::DOWN:
+                npos.y--;
+            default:
+                break;
+            }
+            if (dir != dir_t::DOWN)
+            {
+                if (!is_blocked_movement(npos, *cave))
+                    pos = npos;
+            }
+            else
+            {
+                if (!collision_rock(npos, *cave))
+                    pos = npos;
+                else
+                {
+                    frozen = true;
+                    setFrozenPos(cave);
+                }
             }
         }
     }
-    void update_floor(map<int32_t, int32_t> *cfloor)
-    {
-        if (frozen)
-        {
-            vector<coord_t> shape = shape_map[rock];
-            for (auto it : shape)
-                if (cfloor->at(it.x + pos.x) < it.y + pos.y)
-                    cfloor->at(it.x + pos.x) = it.y + pos.y;
-        }
-    }
-    void move_rock(dir_t dir, cave_t *cave, map<int32_t, int32_t> *cfloor)
-    {
-        coord_t npos = pos;
-        switch (dir)
-        {
-        case dir_t::LEFT:
-            npos.x--;
-            break;
-        case dir_t::RIGHT:
-            npos.x++;
-            break;
-        case dir_t::DOWN:
-            npos.y--;
-        default:
-            break;
-        }
-        npos = block_by_wall(npos);
-        // if (!collision_rock(npos, *cave))
-        npos = block_by_frozen(npos, dir, *cfloor);
-        if (!collision_floor(npos, dir, *cfloor))
-            pos = npos;
-        else
-        {
-            frozen = true;
-            // setFrozenPos(cave);
-            update_floor(cfloor);
-        }
-    }
-    bool isFrozen(void) { return frozen; }
-    coord_t getPos(void) { return pos; }
-    int32_t getHighest(void) { return pos.y + grid_size[rock].y - 1; }
 };
 
 /** used in processing */
@@ -242,85 +267,71 @@ void write_output(const char *filename, int32_t task1, int32_t task2)
 void process_task()
 {
     /** Do something to process the task in general */
-    for (auto it : *data)
-        cout << dir_to_char[it];
-    cout << "\n";
 }
 int32_t process_task1(void)
 {
-    /** Do something to process the task specific to 1 */
     /** init cave with a floor */
-    cave_t *cave = new cave_t();
-    map<int32_t, int32_t> *cfloor = new map<int32_t, int32_t>();
+    size_t rcnt = 2023;
+    cave_t *cave = new cave_t(8);
     for (int32_t x = 0; x < 7; x++)
     {
-        (*cave)[x][0] = true;
-        (*cfloor)[x] = 0;
-        // cave->at(x)[0] = true;
+        cave->at(x).resize(rcnt * 4, false);
+        cave->at(x)[0] = true;
     }
-    coord_t nextSpawn = {2, 5};
+    coord_t nextSpawn = {2, 4};
     rock_t rock_shape = rock_t::HLINE;
     list<ROCK> rocks;
     rocks.push_back(ROCK(rock_shape, nextSpawn));
     ROCK current_rock = rocks.back();
-    bool next_down = true;
+    bool next_down = false;
     int32_t ctr = 0;
-    size_t rcnt = 2022;
+    int32_t highest_point = 0;
     while (rocks.size() < rcnt)
     {
-        cout << "current position: " << current_rock.getPos().x << ", " << current_rock.getPos().y;
         if (next_down)
         {
-            current_rock.move_rock(dir_t::DOWN, cave, cfloor);
+            current_rock.move_rock(dir_t::DOWN, cave);
             next_down = false;
-            cout << " moving DOWN\n";
         }
         else
         {
-            cout << " moving " << (data->at(ctr) == dir_t::LEFT ? "LEFT" : "RIGHT") << "\n";
-            current_rock.move_rock(data->at(ctr), cave, cfloor);
-            ctr++;
+            current_rock.move_rock(data->at(ctr++), cave);
+            // ctr++;
             next_down = true;
         }
-        // if (current_rock.isFrozen() && rocks.size() >= rcnt)
-        //     break;
         if (current_rock.isFrozen())
         {
-            next_down = true;
+            // update highest point, keep old if current rock fell below
+            highest_point = current_rock.getHighest() < highest_point ? highest_point : current_rock.getHighest();
+            // update spawn position
             nextSpawn.x = 2;
-            nextSpawn.y = current_rock.getHighest() + 5;
+            nextSpawn.y = highest_point + 4;
+            // update rock type
             rock_shape = (rock_t)((int32_t)rock_shape + 1);
             if (rock_shape == rock_t::NONE)
                 rock_shape = rock_t::HLINE;
+            // store rock
             rocks.push_back(ROCK(rock_shape, nextSpawn));
+            // pick current rock
             current_rock = rocks.back();
-            cout << "rocks spawned: " << rocks.size() << ", new rock: ";
-            cout << rock_shape << " at ";
-            cout << current_rock.getPos().x << ", " << current_rock.getPos().y << "\n";
+            // next movement is down
+            next_down = false;
         }
         ctr %= cnt;
     }
-    int32_t highest_point = 0;
-    for (auto it : *cfloor)
-        highest_point = it.second > highest_point ? it.second : highest_point;
-    cout << current_rock.getPos().x << ", " << current_rock.getPos().y << "\n";
-    for (int32_t y = highest_point; y >= 0; y--)
-    {
-        for (int32_t x = 0; x < 7; x++)
-        {
-
-            if (cfloor->at(x) == y)
-                cout << "#";
-            else if (cave->at(x).count(y) != 0)
-                cout << "#";
-            else
-                cout << ".";
-        }
-        cout << "\n";
-    }
+    // for (int32_t y = highest_point; y >= 0; y--)
+    // {
+    //     for (int32_t x = 0; x < 7; x++)
+    //     {
+    //         if (cave->at(x)[y])
+    //             cout << "#";
+    //         else
+    //             cout << ".";
+    //     }
+    //     cout << "\n";
+    // }
 
     delete cave;
-    delete cfloor;
     return highest_point;
 }
 int32_t process_task2(void)
@@ -331,10 +342,10 @@ int32_t process_task2(void)
 int main()
 {
     /** use this for testing examples */
-    read_input("test.txt");
+    // read_input("test.txt");
 
     /** use this to run input */
-    // read_input("input.txt");
+    read_input("input.txt");
 
     process_task();
 
