3ccdaafc42fc4b261e71828519ca4a544fe82f02
vakuj <vakujrvi@gmail.com>
2022-12-28 21:30:47 +0100

    D17 task 2 done.

--- d17/main.cpp
+++ d17/main.cpp
@@ -5,13 +5,16 @@
 #include <array>
 #include <map>
 #include <list>
+#include <bitset>
+#include <cstring>
+#include <cassert>
 
 using namespace std;
 
 #define BUFSIZE 1024
 #define WIDTH 8
-#define HEIGHT 4096
-#define cave_t array<array<bool, HEIGHT>, WIDTH>
+#define HEIGHT 8192
+#define cave_t array<bitset<WIDTH>, HEIGHT>
 typedef struct coord_t
 {
     int32_t x;
@@ -138,7 +141,7 @@ public:
             return true;
         // blocked by other frozen rock?
         for (auto it : shape_map[rock])
-            if (cave[it.x + npos.x][it.y + npos.y])
+            if (cave[it.y + npos.y].test(it.x + npos.x))
                 return true;
 
         return false;
@@ -156,7 +159,7 @@ public:
     {
         for (auto it : shape_map[rock])
         {
-            if (cave[it.x + npos.x][it.y + npos.y])
+            if (cave[it.y + npos.y].test(it.x + npos.x))
                 return true;
         }
         return false;
@@ -170,7 +173,7 @@ public:
     {
         frozen = true;
         for (auto it : shape_map[rock])
-            cave->at(it.x + pos.x)[it.y + pos.y] = true;
+            cave->at(it.y + pos.y).set(it.x + pos.x);
     }
     /**
      * @brief Moves the rock based on dir and updates cave accordingly. Does nothing if rock already frozen.
@@ -270,184 +273,282 @@ void process_task()
 {
     /** Do something to process the task in general */
 }
-int32_t clear_floor(cave_t *cave, size_t highest_point)
+/**
+ * @brief Moves the rock in accordance to the game
+ *
+ * @param rock pointer to the rock to move
+ * @param down true for moving down, pointed to value is flipped upon use, and set to false if rock is frozen.
+ * @param move_ctr the index of the next movement direction from data. is incremented and mod-ed to the cnt of movements in data.
+ * @param cave pointer to where the movement should occur.
+ * @return true if the rock froze due to the current movement, else false.
+ */
+bool make_move(ROCK *rock, bool *down, int32_t *move_ctr, cave_t *cave)
 {
-    size_t count = 0;
-    int32_t offset = 0;
-    for (size_t y = highest_point; y > 0; y -= 2)
+    if (*down)
     {
-        if (cave->at(0)[y])
-        {
-            count = 1;
-            for (size_t x = 1; x < WIDTH; x++)
-                count += cave->at(x)[y] ? 1 : 0; //(cave->at(x)[y - 1] ? 1 : 0);
-        }
-        if (count >= WIDTH - 2)
-        {
-            offset = (int32_t)(y - 1);
-            break;
-        }
+        rock->move_rock(dir_t::DOWN, cave);
+        *down = false;
     }
-    if (offset > 0)
+    else
     {
-        // cout << offset << ", " << is_prime(offset) << endl;
-        array<bool, HEIGHT> *tmp = new array<bool, HEIGHT>();
-        for (size_t i = 0; i < WIDTH; i++)
-        {
-            tmp->fill(false);
-            copy(cave->at(i).begin() + offset, cave->at(i).end(), tmp->begin());
-            copy(tmp->begin(), tmp->end(), cave->at(i).begin());
-        }
-        delete tmp;
+        rock->move_rock(data->at((*move_ctr)++), cave);
+        *move_ctr %= cnt;
+        *down = true;
     }
-
-    return offset;
+    if (rock->isFrozen())
+    {
+        *down = false;
+        return true;
+    }
+    return false;
 }
-
+/**
+ * @brief Spawns a new rock for the pointer rock, according to the next rock_shape. highest_point is evaluted and updated if needed.
+ *
+ * @param rock pointer to the rock to be spawned
+ * @param rock_shape pointer to the rock_shape which is incremented and mod-ed to real rock shapes.
+ * @param highest_point pointer to the highest_pointed, by the current rock before spawned if new high point is reached.
+ */
+void spawn_new_rock(ROCK *rock, rock_t *rock_shape, int32_t *highest_point)
+{
+    // update highest point
+    *highest_point = rock->getHighest() < *highest_point ? *highest_point : rock->getHighest();
+    // calculate next spawn position
+    coord_t nextSpawn = {2, *highest_point + 4};
+    // update rock shape
+    *rock_shape = (rock_t)((int32_t)*rock_shape + 1);
+    if (*rock_shape == rock_t::NONE)
+        *rock_shape = rock_t::HLINE;
+    // make new rock
+    *rock = ROCK(*rock_shape, nextSpawn);
+}
+/**
+ * @brief returns true if repeating pattern of pattern_size length and at offset is found
+ *
+ * @param cave pointer to where to check if pattern is found
+ * @param offset from the start where to search for pattern
+ * @param pattern_size the length of the pattern
+ * @return true if pattern is detected
+ */
+inline bool hasPattern(cave_t *cave, int32_t offset, int32_t pattern_size)
+{
+    return (0 == memcmp(cave->data() + offset, cave->data() + offset + pattern_size, pattern_size));
+}
+/**
+ * @brief searches for next appearance of element at cave[offset] starting at start+1
+ *
+ * @param cave where to search for next element
+ * @param offset to the element to search for
+ * @param start starting point (+1) where to begin the search (default 0)
+ * @return int32_t the number of element iterated through before found. (-1 on failure)
+ */
+inline int32_t patternSize(cave_t *cave, int32_t offset, int32_t start = 0)
+{
+    for (int32_t i = start + 1; i < HEIGHT / 2; i++)
+    {
+        if (cave->at(offset) == cave->at(offset + i))
+            return i;
+    }
+    return -1;
+}
+/**
+ * @brief finds first occurance of repeating pattern, requires 3 repeats
+ *
+ * @param cave pointer to where to search for pattern
+ * @param pattern_size pointer to a hint of the lower bound for size of pattern to search for, is updated if a more suitable length is found.
+ * @return int32_t the offset to the first pattern start from beginning (-1 on failure)
+ */
+int32_t searchPattern(cave_t *cave, int32_t *pattern_size)
+{
+    int32_t offset = 0;
+    if (*pattern_size <= 0)
+        *pattern_size = 0;
+    int32_t poff = 0; // pattern offset
+    while ((offset + *pattern_size) < HEIGHT / 2)
+    {
+        poff = *pattern_size;                   // initialize pattern size at hint
+        poff = patternSize(cave, offset, poff); // find in
+        while (poff >= 0)
+        {
+            if (poff > 1) // ignore <= 1 search patterns
+            {
+                // check that (this and next) and (next and next+) are the same pattern
+                if (hasPattern(cave, offset, poff) && hasPattern(cave, offset + poff, poff))
+                {
+                    *pattern_size = poff;
+                    return offset;
+                }
+            }
+            poff = patternSize(cave, offset, poff); // find the next pattern offset
+        }
+        offset++;
+    }
+    return -1;
+}
+/**
+ * @brief Prints the content of the cave for every row which contains any rock elements
+ *
+ * @param cave to print
+ */
+void print_cave(cave_t cave)
+{
+    for (size_t y = HEIGHT - 1; y > 0; y--)
+    {
+        if (cave[y - 1].any())
+        {
+            for (int32_t x = 0; x < 7; x++)
+            {
+                if (cave[y - 1].test(x))
+                    cout << "#";
+                else
+                    cout << ".";
+            }
+            cout << " : " << y - 1 << "\n";
+        }
+    }
+}
 size_t process_task1(void)
 {
+    size_t rcnt = 2022;
     /** init cave with a floor */
-    size_t rcnt = 2023;
     cave_t *cave = new cave_t();
-    for (int32_t x = 0; x < 7; x++)
-    {
-        cave->at(x).fill(false);
-        cave->at(x)[0] = true;
-    }
+    cave->at(0).set();
     coord_t nextSpawn = {2, 4};
     rock_t rock_shape = rock_t::HLINE;
-    list<ROCK> rocks;
-    rocks.push_back(ROCK(rock_shape, nextSpawn));
-    ROCK current_rock = rocks.back();
-    bool next_down = false;
-    int32_t ctr = 0;
     int32_t highest_point = 0;
+    int32_t ctr = 0;
+    bool next_down = false;
+    ROCK current_rock = ROCK(rock_shape, nextSpawn);
+    list<ROCK> rocks;
     while (rocks.size() < rcnt)
     {
-        if (next_down)
+        if (make_move(&current_rock, &next_down, &ctr, cave))
         {
-            current_rock.move_rock(dir_t::DOWN, cave);
-            next_down = false;
+            rocks.push_back(current_rock);
+            spawn_new_rock(&current_rock, &rock_shape, &highest_point);
         }
-        else
-        {
-            current_rock.move_rock(data->at(ctr++), cave);
-            // ctr++;
-            next_down = true;
-        }
-        if (current_rock.isFrozen())
-        {
-            // update highest point, keep old if current rock fell below
-            highest_point = current_rock.getHighest() < highest_point ? highest_point : current_rock.getHighest();
-            // update spawn position
-            nextSpawn.x = 2;
-            nextSpawn.y = (int32_t)highest_point + 4;
-
-            // update rock type
-            rock_shape = (rock_t)((int32_t)rock_shape + 1);
-            if (rock_shape == rock_t::NONE)
-                rock_shape = rock_t::HLINE;
-
-            // store rock
-            rocks.push_back(ROCK(rock_shape, nextSpawn));
-            // pick current rock
-            current_rock = rocks.back();
-            // next movement is down
-            next_down = false;
-        }
-        ctr %= cnt;
     }
-    // for (size_t y = highest_point; y > 0; y--)
-    // {
-    //     for (int32_t x = 0; x < 7; x++)
-    //     {
-    //         if (cave->at(x)[y - 1])
-    //             cout << "#";
-    //         else
-    //             cout << ".";
-    //     }
-    //     cout << "\n";
-    // }
-
+    /** uncomment to print cave */
+    // print_cave(*cave);
     delete cave;
     return highest_point;
 }
-size_t process_task2(void)
+/**
+ * @brief Idea: First simulate many rocks falling. From the many rocks cave, extract repeating pattern including also the preamble
+ * before the first occurance of the pattern. Then calculate the how many patterns are needed for the big rock tower. From that extract
+ * the height and also add the height for the preamble and any partial pattern heights.
+ *
+ * @param psize hint to the lower bound of expected pattern length. 40 works for test case, and 2000 works for the real case.
+ * @return size_t the rock tower height
+ */
+size_t process_task2(int32_t psize)
 {
+#define ROCKMANY 10000
+#define ROCKMAX 1000000000000
+
+    size_t rock_count = ROCKMANY; // start by simulating many rocks
+
     /** init cave with a floor */
-    // size_t rock_count = 1000000000000;
-
-#define ROCKBIG 1000000000000
-#define ROCKMAX 2022
-
-    size_t rock_count = ROCKMAX;
     cave_t *cave = new cave_t();
-    for (int32_t x = 0; x < 7; x++)
-    {
-        cave->at(x).fill(false);
-        cave->at(x)[0] = true;
-    }
+    cave->at(0).set();
+
     coord_t nextSpawn = {2, 4};
     rock_t rock_shape = rock_t::HLINE;
 
     ROCK current_rock = ROCK(rock_shape, nextSpawn);
     bool next_down = false;
     int32_t ctr = 0;
-    size_t highest_point = 0;
-    size_t floor_offset = 0;
-    size_t prev_floor = 0;
-    size_t prev_rock = rock_count;
+    int32_t highest_point = 0;
+    int32_t poffset = 0;
     while (rock_count > 0)
     {
-        if (next_down)
+        if (make_move(&current_rock, &next_down, &ctr, cave))
         {
-            current_rock.move_rock(dir_t::DOWN, cave);
-            next_down = false;
-        }
-        else
-        {
-            current_rock.move_rock(data->at(ctr++), cave);
-            ctr %= cnt;
-            next_down = true;
-        }
-        if (current_rock.isFrozen())
-        {
-            // update highest point, keep old if current rock fell below
+            spawn_new_rock(&current_rock, &rock_shape, &highest_point);
+            rock_count--;
             if (highest_point > HEIGHT - 100)
             {
-                auto current_floor_offset = clear_floor(cave, highest_point);
-                highest_point -= current_floor_offset;
-
-                // auto current_highest = (size_t)(current_rock.getHighest() - current_floor_offset);
-
-                floor_offset += current_floor_offset;
-
-                // cout << ctr << ": " << ROCKMAX - rock_count << " : " << highest_point + floor_offset << " --> ";
-                // cout << prev_rock - rock_count << " : " << (highest_point + floor_offset - prev_floor) << endl;
-                // prev_rock = rock_count;
-                // prev_floor = highest_point + floor_offset;
+                // with many rocks added try find a pattern
+                poffset = searchPattern(cave, &psize);
+                break;
             }
-            else
-                highest_point = (size_t)(current_rock.getHighest()) < highest_point ? highest_point : (size_t)(current_rock.getHighest());
+        }
+    }
+    assert(poffset != -1);                                    // -1 indicates failure to find pattern
+    cave_t *preamble = new cave_t();                          // for the preamble
+    cave_t *pattern = new cave_t();                           // for the pattern
+    copy_n(cave->begin(), poffset, preamble->begin());        // copy of the cave layout for later comparison with the preamble
+    copy_n(cave->begin() + poffset, psize, pattern->begin()); // copy of the cave layout for later comparison with the pattern
 
-            // update spawn position
-            nextSpawn.x = 2;
-            nextSpawn.y = (int32_t)highest_point + 4;
+    /** uncomment for print pattern and preamble */
+    // cout << "preamble\n";
+    // print_cave(*preamble);
+    // cout << "repeating pattern\n";
+    // print_cave(*pattern);
 
-            // update rock type
-            rock_shape = (rock_t)((int32_t)rock_shape + 1);
-            if (rock_shape == rock_t::NONE)
-                rock_shape = rock_t::HLINE;
-            // update current rock
-            current_rock = ROCK(rock_shape, nextSpawn);
-            rock_count--;
-            // next movement is not down
-            next_down = false;
+    // clear the cave to restart the rock falling
+    for (auto it = cave->begin(); it != cave->end(); it++)
+        it->reset();
+    cave->at(0).set();
+
+    // re-initialize all to starting point
+    ctr = 0;
+    rock_shape = rock_t::HLINE;
+    nextSpawn = {2, 4};
+    next_down = false;
+    current_rock = ROCK(rock_shape, nextSpawn);
+    highest_point = 0;
+
+    list<ROCK> first_part; // contains the rocks for preamble
+    list<ROCK> repeat;     // contains the rocks for the repeating pattern
+
+    while (true)
+    {
+        if (make_move(&current_rock, &next_down, &ctr, cave))
+        {
+            first_part.push_back(current_rock); // store preamble rocks for later use
+            spawn_new_rock(&current_rock, &rock_shape, &highest_point);
+            if (equal(preamble->begin(), preamble->begin() + poffset, cave->begin())) // check if preamble is complete
+                break;
+        }
+    }
+    while (true)
+    {
+        if (make_move(&current_rock, &next_down, &ctr, cave))
+        {
+            repeat.push_back(current_rock); // store repeating rocks for later use
+            spawn_new_rock(&current_rock, &rock_shape, &highest_point);
+            if (equal(pattern->begin(), pattern->begin() + psize, cave->begin() + poffset)) // check if pattern is complete
+                break;
         }
     }
 
+    size_t rock_tower_height = 0;                                 // init rock tower height at 0
+    size_t rocks = ROCKMAX - first_part.size();                   // remove rocks from preamble
+    rock_tower_height += (size_t)psize * (rocks / repeat.size()); // multiply pattern size with amount of repetitions and add to height
+    rocks %= repeat.size();                                       // take the remainder of the height
+    if (rocks == 0)
+    {
+        // add the pattern offset to the height if no remainder
+        rock_tower_height += (size_t)poffset - 1;
+    }
+    else
+    {
+        // add the remaining rocks, height includes the pattern offset
+        highest_point = 0;
+        auto it = repeat.begin();
+        while (rocks > 0)
+        {
+            highest_point = it->getHighest() < highest_point ? highest_point : it->getHighest();
+            it++;
+            rocks--;
+        }
+        rock_tower_height += (size_t)highest_point;
+    }
     delete cave;
-    return highest_point + floor_offset;
+    delete pattern;
+    delete preamble;
+    return rock_tower_height;
 }
 int main()
 {
@@ -460,7 +561,12 @@ int main()
     process_task();
 
     size_t task1 = process_task1();
-    size_t task2 = process_task2();
+
+    /** use this for the testing data set */
+    // size_t task2 = process_task2(40);
+
+    /** use this for the real data set */
+    size_t task2 = process_task2(2000);
 
     /** store output to file */
     write_output("output.txt", task1, task2);
