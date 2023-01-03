8401e8ee82e35f3fee6f0ba2ad94119b1f3e05d2
vakuj <vakujrvi@gmail.com>
2023-01-03 22:13:38 +0100

    D20 task 2 wip.

--- d20/main.cpp
+++ d20/main.cpp
@@ -12,8 +12,9 @@ using namespace std;
 /** used in processing */
 typedef struct item_t
 {
-    int32_t value;
+    int64_t value;
     int32_t touched;
+    int32_t id;
 } item_t;
 
 list<item_t> *data;
@@ -34,7 +35,7 @@ void read_input(const char *filename)
     ifs.seekg(0, ifstream::beg);
 
     data = new list<item_t>();
-    item_t item = {0, 0};
+    item_t item = {0, 0, 0};
     char *indata = new char[BUFSIZE];
     int32_t ctr = 0;
     while (!ifs.eof())
@@ -49,10 +50,10 @@ void read_input(const char *filename)
         {
             ifs.getline(indata, BUFSIZE);
             /** store for later processing */
-            if (1 == sscanf(indata, "%d", &item.value))
+            if (1 == sscanf(indata, "%ld", &item.value))
             {
+                item.id = ctr++;
                 data->push_back(item);
-                ctr++;
             }
         }
     }
@@ -61,7 +62,7 @@ void read_input(const char *filename)
     delete[] indata;
 }
 
-void write_output(const char *filename, int32_t task1, int32_t task2)
+void write_output(const char *filename, int64_t task1, int64_t task2)
 {
     ofstream ofs(filename, ofstream::out);
     ofs << "Task 1 result: " << task1 << "\n";
@@ -77,24 +78,51 @@ void print_list(list<item_t> *items)
     }
     cout << endl;
 }
-inline int32_t truncate_index(int32_t position, int32_t index)
+inline int64_t truncate_index(int64_t from, int64_t index)
 {
-    index += position;
-    if (index > cnt)
+    index %= cnt;
+    cout.width(3);
+    cout << index << ", ";
+    index += from;
+    if (index >= cnt)
         return (index + 1) % cnt;
-    if (index < -cnt)
+    if (index <= -cnt)
         return (index - 2) % cnt;
     if (index <= 0)
         return (index - 1) % cnt;
     return index;
 }
-void move_item(list<item_t> *items, item_t item, int32_t from)
+void move_item(list<item_t> *items, item_t item, int64_t from)
 {
-    int32_t index = truncate_index(from, item.value);
-    item.touched += 1;
+    int64_t index = truncate_index(from, item.value);
+    cout.width(12);
+    cout << item.value << ", ";
+    cout.width(5);
+    cout << from << ", ";
+    cout.width(5);
+    cout << index << endl;
     auto it = items->begin();
     advance(it, index);
     items->insert(it, item);
+}
+int32_t find_next_id(int32_t id, list<item_t> *items, item_t *item)
+{
+    auto it = items->begin();
+    int32_t index = 0;
+    while (it != items->end())
+    {
+        if (it->id == id)
+        {
+            it->touched += 1;
+            *item = *it;
+            if (it->value != 0)
+                items->erase(it);
+            return index;
+        }
+        it++;
+        index++;
+    }
+    return -1;
 }
 int32_t find_next(list<item_t> *items, item_t *item)
 {
@@ -104,9 +132,8 @@ int32_t find_next(list<item_t> *items, item_t *item)
     {
         if (!it->touched)
         {
-            if (it->value == 0) // only touch zeros, as they will not move anywhere
-                it->touched += 1;
-            else
+            it->touched += 1;
+            if (it->value != 0) // only touch zeros, as they will not move anywhere
             {
                 *item = *it;
                 items->erase(it);
@@ -129,7 +156,7 @@ int32_t find_zero(list<item_t> *items)
     }
     return -1;
 }
-int32_t first_non_zero(list<item_t> *items, int32_t index)
+int64_t first_non_zero(list<item_t> *items, int32_t index)
 {
     auto it = items->begin();
     index %= cnt;
@@ -146,21 +173,16 @@ void decrypt(list<item_t> *items)
 {
     const int32_t key = 811589153;
     for (auto it = items->begin(); it != items->end(); it++)
-    {
         it->value *= key;
-    }
 }
 void process_task()
 {
     /** Do something to process the task in general */
 }
-int32_t process_task1(void)
+int64_t process_task1(void)
 {
-    /** Do something to process the task specific to 1 */
-    // print_list(data);
     list<item_t> *items = new list<item_t>();
     items->assign(data->begin(), data->end());
-
     int32_t index = 0;
     item_t item;
     while (true)
@@ -170,41 +192,57 @@ int32_t process_task1(void)
             break;
         if (item.value != 0)
             move_item(items, item, index);
-        // print_list(data);
     }
+    print_list(items);
     index = find_zero(items);
     assert(index >= 0);
-    int32_t i1 = first_non_zero(items, index + 1000);
-    int32_t i2 = first_non_zero(items, index + 2000);
-    int32_t i3 = first_non_zero(items, index + 3000);
+    int64_t i1 = first_non_zero(items, index + 1000);
+    int64_t i2 = first_non_zero(items, index + 2000);
+    int64_t i3 = first_non_zero(items, index + 3000);
 
-    cout << index << ": " << i1 << ", " << i2 << ", " << i3 << endl;
+    // cout << index << ": " << i1 << ", " << i2 << ", " << i3 << endl;
     delete items;
     return i1 + i2 + i3;
 }
-int32_t process_task2(void)
+int64_t process_task2(void)
 {
-    /** Do something to process the task specific to 2 */
     list<item_t> *items = new list<item_t>();
     items->assign(data->begin(), data->end());
-    // decrypt(items);
-    cout << uset.size() << ", " << items->size() << endl;
+    decrypt(items);
+    int32_t index = 0;
+    item_t item;
+    print_list(items);
+    for (int32_t id = 0; id < (int32_t)items->size(); id++)
+    {
+        index = find_next_id(id, items, &item);
+        assert(index >= 0);
+        if (item.value != 0)
+            move_item(items, item, index);
+    }
+    print_list(items);
+    index = find_zero(items);
+    assert(index >= 0);
+    int64_t i1 = first_non_zero(items, index + 1000);
+    int64_t i2 = first_non_zero(items, index + 2000);
+    int64_t i3 = first_non_zero(items, index + 3000);
+    cout << index << ": " << i1 << ", " << i2 << ", " << i3 << endl;
 
     delete items;
-    return 0;
+    return i1 + i2 + i3;
 }
 int main()
 {
     /** use this for testing examples */
-    // read_input("test.txt");
+    read_input("test.txt");
 
     /** use this to run input */
-    read_input("input.txt");
+    // read_input("input.txt");
 
     process_task();
 
-    int32_t task1 = process_task1();
-    int32_t task2 = process_task2();
+    int64_t task1 = process_task1();
+    // int64_t task1 = 0;
+    int64_t task2 = process_task2();
 
     /** store output to file */
     write_output("output.txt", task1, task2);
