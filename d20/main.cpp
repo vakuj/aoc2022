ea6550ff9351e003dc55777a213fea9e4e444955
vakuj <vakujrvi@gmail.com>
2023-01-03 15:30:32 +0100

    D20 task 1 done. D20 task 2 started.

--- d20/main.cpp
+++ d20/main.cpp
@@ -3,15 +3,18 @@
 #include <algorithm>
 #include <vector>
 #include <list>
+#include <set>
+#include <cassert>
 
 using namespace std;
 
 #define BUFSIZE 1024
+
 /** used in processing */
 typedef struct item_t
 {
     int32_t value;
-    bool touched;
+    int32_t touched;
 } item_t;
 
 list<item_t> *data;
@@ -32,7 +35,7 @@ void read_input(const char *filename)
     ifs.seekg(0, ifstream::beg);
 
     data = new list<item_t>();
-    item_t item = {0, false};
+    item_t item = {0, 0};
     char *indata = new char[BUFSIZE];
     int32_t ctr = 0;
     while (!ifs.eof())
@@ -75,38 +78,39 @@ void print_list(list<item_t> *items)
     }
     cout << endl;
 }
-int32_t truncate_index(int32_t position, int32_t index)
+inline int32_t truncate_index(int32_t position, int32_t index)
 {
-    index = position + index;
-
-    if (index < 0)
-        return (cnt - 1 + index);
-    if (index > 0)
-        return index % cnt;
+    index += position;
+    if (index > cnt)
+        return (index + 1) % cnt;
+    if (index < -cnt)
+        return (index - 2) % cnt;
+    if (index <= 0)
+        return (index - 1) % cnt;
     return index;
 }
-void move_item(item_t item, int32_t from)
+void move_item(list<item_t> *items, item_t item, int32_t from)
 {
     int32_t index = truncate_index(from, item.value);
-    item.touched = true;
-    auto it = data->begin();
+    item.touched += 1;
+    auto it = items->begin();
     advance(it, index);
-    data->insert(it, item);
+    items->insert(it, item);
 }
-int32_t find_next(item_t *item)
+int32_t find_next(list<item_t> *items, item_t *item)
 {
-    auto it = data->begin();
+    auto it = items->begin();
     int32_t index = 0;
-    while (it != data->end())
+    while (it != items->end())
     {
         if (!it->touched)
         {
             if (it->value == 0) // only touch zeros, as they will not move anywhere
-                it->touched = true;
+                it->touched += 1;
             else
             {
                 *item = *it;
-                data->erase(it);
+                items->erase(it);
                 return index;
             }
         }
@@ -115,6 +119,38 @@ int32_t find_next(item_t *item)
     }
     return -1;
 }
+int32_t find_zero(list<item_t> *items)
+{
+    list<item_t>::iterator it;
+    int32_t i;
+    for (it = items->begin(), i = 0; it != items->end(); i++, it++)
+    {
+        if (it->value == 0)
+            return i;
+    }
+    return -1;
+}
+int32_t first_non_zero(list<item_t> *items, int32_t index)
+{
+    auto it = items->begin();
+    index %= cnt;
+    advance(it, index);
+    while (it->value == 0)
+    {
+        it++;
+        if (it == items->end())
+            it = items->begin();
+    }
+    return it->value;
+}
+void decrypt(list<item_t> *items)
+{
+    const int32_t key = 811589153;
+    for (auto it = items->begin(); it != items->end(); it++)
+    {
+        it->value *= key;
+    }
+}
 void process_task()
 {
     /** Do something to process the task in general */
@@ -122,33 +158,52 @@ void process_task()
 int32_t process_task1(void)
 {
     /** Do something to process the task specific to 1 */
-    print_list(data);
+    // print_list(data);
+    list<item_t> *items = new list<item_t>();
+    items->assign(data->begin(), data->end());
+
     int32_t index = 0;
     item_t item;
     while (true)
     {
-        index = find_next(&item);
+        index = find_next(items, &item);
         if (index < 0)
             break;
         if (item.value != 0)
-            move_item(item, index);
-        print_list(data);
+            move_item(items, item, index);
+        // print_list(data);
     }
-    return 0;
+    index = find_zero(items);
+    assert(index >= 0);
+    int32_t i1 = first_non_zero(items, index + 1000);
+    int32_t i2 = first_non_zero(items, index + 2000);
+    int32_t i3 = first_non_zero(items, index + 3000);
+
+    cout << index << ": " << i1 << ", " << i2 << ", " << i3 << endl;
+    delete items;
+    return i1 + i2 + i3;
 }
 int32_t process_task2(void)
 {
     /** Do something to process the task specific to 2 */
+    list<item_t> *items = new list<item_t>();
+    items->assign(data->begin(), data->end());
+    // decrypt(items);a
+    set<int32_t> uset;
+    for (auto it : *items)
+        uset.insert(it.value);
+    cout << uset.size() << ", " << items->size() << endl;
 
+    delete items;
     return 0;
 }
 int main()
 {
     /** use this for testing examples */
-    read_input("test.txt");
+    // read_input("test.txt");
 
     /** use this to run input */
-    // read_input("input.txt");
+    read_input("input.txt");
 
     process_task();
 
